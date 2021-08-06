9a9a7dd8aba76831f5ae856e1d19e6cbe362d006
Harit Kumar <haritkumar@hotmail.com>
2021-08-06 20:42:52 +0530

    conversions

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from translate import Translator
+import os.path
 
 
 def convert(target_lang):
     translator = Translator(to_lang=target_lang)
-    translation = translator.translate("This is a pen.")
-    print(translation)
+    relative_path = os.path.abspath(os.path.dirname(__file__))
+    fw = open(relative_path + '/files/translations/' + target_lang + '.properties', "w")
+    with open(relative_path + '/files/en.properties') as f:
+        for line in f:
+            splited = line.split('=')
+            converted = splited[0] + "=" + translator.translate(splited[1])
+            print(converted)
+            fw.write(converted + "\n")
+    fw.close()
 
 
 if __name__ == '__main__':
-    convert('zh')
+    convert('hi')
