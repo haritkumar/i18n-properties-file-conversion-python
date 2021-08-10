d4f65ee28dce44d2b5ae36774f7da2e882b9f6f0
Harit Kumar <haritkumar@hotmail.com>
2021-08-10 19:11:29 +0530

    updating main

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ def convert(target_lang):
     relative_path = os.path.abspath(os.path.dirname(__file__))
     fw = open(relative_path + '/files/translations/' + target_lang + '.properties', "w", encoding="utf-8")
     with open(relative_path + '/files/en.properties') as f:
+        i = 1
         for line in f:
             splited = line.split('=')
-            translated = translator.translate(splited[1], dest=target_lang)
-            converted = splited[0] + "=" + translated.text
-            print(converted)
-            fw.write(converted + "\n")
+            translated_text = ''
+            if len(splited) == 2:
+                try:
+                    translated = translator.translate(splited[1], dest=target_lang)
+                    translated_text = splited[0] + "=" + translated.text
+                except IndexError as ie:
+                    translated_text = splited[0] + "="
+
+            print(str(i) + '> ' + translated_text)
+            fw.write(translated_text + "\n")
+            i = i + 1
     fw.close()
 
 
